94f676260f6c0ee9d95a0d56b2767769cb9a2a63
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2025-03-17 20:18:13 -0300

    exercicio 10

---
+++ Aula01/Exercicio10.py
@@ -0,0 +1,10 @@
+ttEleitores = int (input("Digite o numero de eleitores: \n"))
+vtBranco = int (input("Digite a quantidade de votos em branco: \n"))
+vtNulo = int (input("Digite a quantidade de votos nulos: \n"))
+vtValido = int (input("Digite o número de votos validados: \n"))
+prcVtBranco = (vtBranco / ttEleitores) * 100, "%"
+prcVtNulo = (vtNulo / ttEleitores) * 100, "%"
+prcVtvalido = (vtValido / ttEleitores) * 100, "%"
+
+print("A quantidade em percentual de brancos é: ", prcVtBranco, "em nulos é: ",
+       prcVtNulo, "e em válidos é: ",prcVtvalido)
