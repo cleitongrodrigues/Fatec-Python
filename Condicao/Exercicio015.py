210c5926880395d800bd60493bd1ead1f2098d8a
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2025-03-24 22:52:02 -0300

    adicionando o //

--- Condicao/Exercicio015.py
+++ Condicao/Exercicio015.py
@@ -1,16 +1,16 @@
 valor = int(input("Informe seu valor de saque: "
 "Lembrando que temos notas de R$10,00 | R$20,00 | R$50,00 e R$100,00\n"))
 if valor % 10 == 0:
-    valor100 = valor / 100
+    valor100 = valor // 100
     sobra100 = valor % 100
 
-    valor50 = sobra100 / 50
+    valor50 = sobra100 // 50
     sobra50 = sobra100 % 50
 
-    valor20 = sobra50 / 20
+    valor20 = sobra50 // 20
     sobra20 = sobra50 % 20
 
-    valor10 = sobra20 / 10
+    valor10 = sobra20 // 10
     print("Notas de R$100,00: ", valor100)
     print("Notas de R$50,00: ", valor50)
     print("Notas de R$20,00: ", valor20)
