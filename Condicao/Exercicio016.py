0d6232954f5cf89f191e70df54d2406bcd9f5897
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2025-03-31 21:34:11 -0300

    exercicios 31-03-2025

---
+++ Condicao/Exercicio016.py
@@ -0,0 +1,5 @@
+letra = input("Digite uma letra para saber se é vogal ou consoante: \n")
+if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+    print("A letra digitada é uma vogal")   
+else:
+    print("A letra digitada é uma consoante")
