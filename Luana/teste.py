6f99fafd61e293ec2080e22dc4fc92b644f1cc68
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2025-05-17 16:44:40 -0300

    a

--- Luana/teste.py
+++ Luana/teste.py
@@ -1,2 +1,9 @@
-nome = str(input("Digite seu nome"))
-print("oi ", nome)
+
+# Peça para o usuário digitar um número.
+
+# Use um laço for para exibir a tabuada desse número, de 1 a 10.
+
+n = int(input("Digite um número para ver sua tabuada"))
+for i in range(0,11):
+    resultado = n * i
+    print("{} * {} = {}".format(n,i,resultado))
